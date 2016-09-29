01e0cc9fc0cc297cb0196083852fdb0b66a5bb8d
Alejandra Villa <codeme@alxvilla.me>
2016-09-29 00:38:11 -0500

    Added reactive class for header based on scroll

--- app/client/templates/header/header.js
+++ app/client/templates/header/header.js
@@ -1,2 +1,15 @@
-Template.header.events({
+Template.header.onCreated(function() {
+	var self = this;
+	self.headerClass = '';
+	self.scroll = new ReactiveVar(0);
+	$(window).on('scroll', function(e) {
+		$(this).scrollTop() > 180 ? headerClass = 'sticky' : headerClass = ''
+		self.scroll.set(headerClass);
+	});
+});
+
+Template.header.helpers({
+	scroll: function() { 
+		return Template.instance().scroll.get(); 
+	}
 });
