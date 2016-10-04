db8d577ab8924759c2ff16c61b1057a653dd2651
Alejandra Villa <codeme@alxvilla.me>
2016-10-04 00:00:26 -0500

    Unified following/followers routes

--- app/client/templates/common/sidePanel.js
+++ app/client/templates/common/sidePanel.js
@@ -2,19 +2,16 @@
 Template.sidePanel.helpers({
 	activeSidePanel: function() {
 		return Session.get('activeSidePanel');
-	},
-  panelType: function () {
-		return Session.get('panelType');
-  }
- });
+	}
+});
 
 // sidePanel events
 Template.sidePanel.events({
 	// Hides the sidepanel
-  'click .cover': function(e){
+	'click .cover': function(e){
 		$(".cover").velocity({ opacity: 0 ,transition:"all 0.1s ease"}, { visibility: "hidden" });
-	  $(".side-panel").velocity({ translateX: ["100%", 0] }, 300);
-	  delete Session.keys['activeSidePanel','panelType'];
+		$(".side-panel").velocity({ translateX: ["100%", 0] }, 300);
+		delete Session.keys['activeSidePanel'];
 		return false;  	
-  }
+	}
 });
