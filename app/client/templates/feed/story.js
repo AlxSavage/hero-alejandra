f5e3fe80e015dfd052b5a40959ab2f79bfee7667
Alejandra Villa <codeme@alxvilla.me>
2016-10-13 23:10:48 -0500

    Added tabs for story and itinerary

--- app/client/templates/feed/story.js
+++ app/client/templates/feed/story.js
@@ -1,10 +1,30 @@
+let templateTabGroupName = 'storyTabs';
+
 Template.story.onCreated(function() {
-   this.storyId = this.data;
+  	this.storyId = this.data;
+    Session.setDefault(templateTabGroupName, 'storyStories');
 });
 
 // Comments helpers, we get the {{storyId}} var from the router
 Template.story.helpers({
-  storyId() {
-    return Template.instance().storyId;
+	storyId() {
+		return Template.instance().storyId;
+	},
+    isActiveTab: function(name) {
+        return Session.equals(templateTabGroupName, name);
+    },
+    activeTabName: function() {
+        return Session.get(templateTabGroupName);
+    },
+    tabKey: function() {
+        return templateTabGroupName;
+    }
+});
+
+Template.story.events({
+  'click .js-tab-trigger': function(e, template){
+    e.preventDefault();
+    let name = template.$(event.target).closest('.js-tab-trigger').data('tab-template');
+    Session.set(templateTabGroupName, name);
   }
 });
