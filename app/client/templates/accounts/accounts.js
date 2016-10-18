35ce4c963c9c6017cea9a192e68ff8f510489fc5
timhannifan <hannifan@gmail.com>
2016-10-17 20:32:15 -0400

    adding back at-title to account pages

--- app/client/templates/accounts/accounts.js
+++ app/client/templates/accounts/accounts.js
@@ -5,5 +5,5 @@ Template['override-atPwdForm'].replaces('atPwdForm');
 Template['override-atSigninLink'].replaces('atSigninLink');
 Template['override-atSignupLink'].replaces('atSignupLink');
 Template['override-atTextInput'].replaces('atTextInput');
-// Template['override-atTitle'].replaces('atTitle');
+Template['override-atTitle'].replaces('atTitle');
 // Template['override-atError'].replaces('atError');
